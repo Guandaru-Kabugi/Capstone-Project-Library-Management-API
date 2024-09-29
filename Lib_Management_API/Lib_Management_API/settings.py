53c2506c7b19eafc0fe16c64428abed3bf4356a9
Guandaru-Kabugi <kabugi96@gmail.com>
2024-09-29 13:31:38 +0300

    New changes to the database

--- Lib_Management_API/Lib_Management_API/settings.py
+++ Lib_Management_API/Lib_Management_API/settings.py
@@ -14,16 +14,18 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from decouple import config
 
 #We get the details from .env
 
-DB_HOST = os.getenv('DB_HOST')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+# DB_HOST = os.getenv('DB_HOST')
+# DB_USER = os.getenv('DB_USER')
+# DB_PASSWORD = os.getenv('DB_PASSWORD')
+#ABOVE ARE APPLICABLE TO MYSQL
 MAIL_API = os.getenv('MAIL_GUN_API')
 DEF_FROM_EMAIL_IS = os.getenv('DEFAULT_FROM_EMAIL')
 MAIL_GUN_DOMAIN = os.getenv('MAILGUN_SENDER_DOMAIN')
-SECRET_DJANGO_KEY = os.getenv('SECRET_KEY')
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,11 +36,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_DJANGO_KEY
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','guandaru.pythonanywhere.com']
 # 'localhost','127.0.0.1','guandaru.pythonanywhere.com'
 
 
